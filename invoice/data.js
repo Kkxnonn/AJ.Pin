63e8a2f56f1ab565a74242e2dd222af81faaaf8d
Kkxnonn <suphitchaya.cc@gmail.com>
2025-09-10 10:37:43 +0700

    save

---
+++ invoice/data.js
@@ -0,0 +1,60 @@
+const data = {
+  // client company
+  companyName: "Richard Sanches",
+  companyAddress: ["123 Anywhere St., Any City, CA 12345"],
+  companyLogo: "logo",
+
+  //pay to
+  payToName: "Borcele Bank",
+  payToEmail: "nSs7r@example.com",
+  payToPhone: "0123 4567 8901",
+
+  //ship to
+  shipToName: "Richard Sanches",
+  shipToAddress: ["123 Anywhere St., Any City, CA 12345"],
+
+  //invoice info
+  invoiceNo: "01234",
+  invoiceDate: "11/02/2030",
+  invoiceDueDate: "11/03/2030",
+
+  //items
+  items: [
+    {
+      quantity: 1,
+      descripation: "Brand consultation",
+      unitPrice: 100,
+    },
+    {
+      quantity: 1,
+      descripation: "Logo design",
+      unitPrice: 100,
+    },
+    {
+      quantity: 1,
+      descripation: "Website design",
+      unitPrice: 100,
+    },
+    {
+      quantity: 1,
+      descripation: "Social media templates",
+      unitPrice: 100,
+    },
+    {
+      quantity: 1,
+      descripation: "Brand photography",
+      unitPrice: 100,
+    },
+    {
+      quantity: 1,
+      descripation: "Brand guide",
+      unitPrice: 100,
+    },
+  ],
+  tax: 10,
+
+  // signature
+  signature: "Richard Sanches",
+
+  // conditions คือเขียนท้ายใบเสร็จว่าชำระภายในกี่วัน
+};
